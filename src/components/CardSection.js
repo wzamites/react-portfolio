8a9d53bbf1a8c4aa06c4957cdd1ceceddfbbb47a
wzamites <bj_zamites@comcast.net>
2019-08-30 22:27:07 -0400

    get repos from the redux store instead of the component state

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import ProjectCard from '../components/ProjectCard'
 import socials from '../assets/socials'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 class CardSection extends React.Component {
-  constructor() {
-      super()
-      this.state = {
-        repos: [],
-      }
-  }
+  // constructor() {
+  //     super()
+  //     this.state = {
+  //       repos: [],
+  //     }
+  // }
+  //
+  // // get your github username from the socials.js asset and make api call
+  // componentDidMount() {
+  //   fetch("https://api.github.com/users/"+ socials.github + "/repos")
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     this.setState({
+  //       //set the state as repos sorted by created_at date
+  //       repos: data.sort(function(a,b) {
+  //         if(b.created_at > a.created_at) {
+  //           return 1
+  //         } else{
+  //           return -1
+  //         }
+  //       })
+  //     })
+  //   })
+  // }
 
-  // get your github username from the socials.js asset and make api call
-  componentDidMount() {
-    fetch("https://api.github.com/users/"+ socials.github + "/repos")
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        repos: data
-      })
-    })
-  }
 
   render() {
 
     //Create this constant which is dupicating ProjectCard over an array
-    const githubCards = this.state.repos.map(
+    const githubCards = this.props.repositories.map(
       function(hub) {
         return (
           <ProjectCard
           key={hub.id}
           desc={hub.description}
-          lang={hub.language}
           name={hub.name}
           url={hub.html_url}
           homepage={hub.homepage}
@@ -51,4 +60,10 @@ class CardSection extends React.Component {
   }
 }
 
-export default CardSection
+function mapStateToProps(state) {
+  return {
+    repositories: state.repositories
+  }
+}
+
+export default connect(mapStateToProps)(CardSection)

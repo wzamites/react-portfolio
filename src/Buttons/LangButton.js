9d4243d081a942bc275917f90b3ccddbb9cd4938
wzamites <bj_zamites@comcast.net>
2019-08-31 18:02:02 -0400

    add redux hookups to select the language pill fill from the redux store

--- src/Buttons/LangButton.js
+++ src/Buttons/LangButton.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 class LangButton extends React.Component {
   render() {
+    //logic to return primary if clicked and outline-primary if not
+    //if the selected skill (props.lang in this case) is true then display primary
+    let buttonDisplay
+    if (this.props.selectedskills[this.props.lang]) {
+      buttonDisplay = "primary"
+    } else {
+      buttonDisplay = "outline-primary"
+    }
+
     return (
-      <Button className="languagePill badge-pill badge" variant="outline-primary">{this.props.lang}</Button>
+      <Button className="languagePill badge-pill badge" variant={buttonDisplay}>{this.props.lang}</Button>
 
     )
   }
 }
 
-export default LangButton
+function mapStateToProps(state) {
+  return {
+    repositories: state.repositories,
+    selectedskills: state.selectedskills
+  }
+}
+
+export default connect(mapStateToProps)(LangButton)

b45aceef3a591ca3ebf5820a9ec145e091443819
wzamites <bj_zamites@comcast.net>
2019-08-30 22:18:30 -0400

    import test component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react'
-
-import './styles/App.css';
-
-// import Test from './components/Test'
 import NavBar from './components/NavBar'
 import Contents from './components/Contents'
 import Footer from './components/Footer'
 import MyHeader from './components/MyHeader'
+// import Test from './components/Test'
+import './styles/App.css';
 
 class App extends React.Component {
   render() {
     return (
-          <div>
-          <NavBar />
-          <MyHeader />
-          <Contents />
-          <Footer />
-          </div>
+        <div>
+        <NavBar />
+        <MyHeader />
+        <Contents />
+        <Footer />
+        </div>
     )
   }
 }
 
-export default App;
+export default App

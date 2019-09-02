c94559f7656601323221c9ede9a555c4cd4952f8
wzamites <bj_zamites@comcast.net>
2019-09-01 23:32:43 -0400

    move api call to single component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import Contents from './components/Contents'
 import Footer from './components/Footer'
 import MyHeader from './components/MyHeader'
+import ApiCalls from './components/ApiCalls'
 // import Test from './components/Test'
 import './styles/App.css';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
   render() {
     return (
         <div>
+        <ApiCalls />
         <NavBar />
         <MyHeader />
         <Contents />
---
+++ src/components/ApiCalls.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { loadGithub } from '../redux/actions/load-github'
+import socials from '../assets/socials'
+
+
+class ApiCalls extends React.Component {
+  componentDidMount() {
+
+    //First call
+    fetch("https://api.github.com/users/"+ socials.github + "/repos", {
+        headers: {
+            "Accept":"application/vnd.github.mercy-preview+json"
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.props.loadGithub(
+        //set the state as repos sorted by created_at date
+        data.sort(function(a,b) {
+          if(b.created_at > a.created_at) {
+            return 1
+          } else{
+            return -1
+          }
+        })
+      )
+    })
+  }
+
+  render() {
+    return null
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    repositories: state.repositories,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({loadGithub:loadGithub}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ApiCalls)

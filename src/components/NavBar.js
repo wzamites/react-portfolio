08d0aa85f00d06e3c7a8fc0d6478abc39340d56d
wzamites <bj_zamites@comcast.net>
2019-09-04 12:17:24 -0400

    put contact navbars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,11 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import Dropdown from 'react-bootstrap/Dropdown'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 class NavBar extends React.Component {
   render () {
@@ -14,6 +19,20 @@ class NavBar extends React.Component {
           <Nav className="mr-auto">
             <Nav.Link href="./dev">Development</Nav.Link>
             <Nav.Link href="./music">Music</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="tel:16109960580">
+                <i class="fas fa-phone"></i>
+            </Nav.Link>
+
+            <Nav.Link href="mailto:wzamites@gmail.com">
+                <i class="fas fa-envelope"></i>
+            </Nav.Link>
+
+            <Nav.Link href="https://drive.google.com/file/d/1crHmK_ONvLLEJnq5oHXtaTZ_m3Wxd1ZZ/view?usp=sharing" target="_blank">
+                <i class="fas fa-file-pdf"></i>
+            </Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>

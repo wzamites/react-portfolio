0016e92072fa6a2a9d8e47f62de12f781ac6e246
wzamites <bj_zamites@comcast.net>
2019-09-04 12:17:35 -0400

    change AND filter to OR

--- src/redux/reducers/skill-array.js
+++ src/redux/reducers/skill-array.js
@@ -6,15 +6,25 @@ export default function(state=defaultState, action) {
   switch(action.type) {
     case "SKILL_TOGGLE":
 
-    //if the skill doesn't exist in the array, put it in there
+    //if the state is double clicked, unselect it.
     if (state.includes(action.payload)) {
-      return state.filter(word => word !== action.payload)
+      return []
 
-    //if the skill does exist in the array, take it out
+    //if the skill is selected, return the skill
     } else {
-      return [...state, action.payload]
+      return [action.payload]
     }
     default:
       return state
   }
 }
+
+//This is the code for AND
+// //if the skill doesn't exist in the array, put it in there
+// if (state.includes(action.payload)) {
+//   return state.filter(word => word !== action.payload)
+//
+// //if the skill does exist in the array, take it out
+// } else {
+//   return [...state, action.payload]
+// }

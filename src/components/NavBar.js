13ca2f40270c80f9686a0f393e54b07e04ec7d39
wzamites <bj_zamites@comcast.net>
2019-09-15 17:59:51 -0400

    make navbar sticky top

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ class NavBar extends React.Component {
   render () {
     return (
 
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar sticky="top" collapseOnSelect expand="md" bg="dark" variant="dark">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">

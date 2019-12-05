0fadd5b6bd2a482db8e169dc4793107808b08029
wzamites <bj_zamites@comcast.net>
2019-12-05 16:39:30 -0500

    only display repositories listed in extraData.js

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -3,24 +3,31 @@ import ProjectCard from '../components/ProjectCard'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { loadGithub } from '../redux/actions/load-github'
+import extraData from '../assets/extraData'
 
 class CardSection extends React.Component {
   render() {
     //Create this constant which is dupicating ProjectCard over an array
     const githubCards = this.props.repositories.map(
       function(hub) {
-        return (
-          <ProjectCard
-          key={hub.id}
-          desc={hub.description}
-          name={hub.name}
-          url={hub.html_url}
-          homepage={hub.homepage}
-          opensource={hub.fork}
-          topics={hub.topics}
-          languages_url={hub.languages_url}
-          />
-        )
+        //if the repo exists in extraData, go ahead
+        //otherwise return null
+        if (extraData.some(e => e.name === hub.name)) {
+          return (
+            <ProjectCard
+            key={hub.id}
+            desc={hub.description}
+            name={hub.name}
+            url={hub.html_url}
+            homepage={hub.homepage}
+            opensource={hub.fork}
+            topics={hub.topics}
+            languages_url={hub.languages_url}
+            />
+          )
+        } else {
+          return null
+        }
       }
     )
 

2e64497cbab0d0152937a843ec04abee827a07e3
wzamites <bj_zamites@comcast.net>
2020-02-15 22:46:01 -0500

    chnage phone button to popover

--- src/Buttons/PhoneButton.js
+++ src/Buttons/PhoneButton.js
@@ -2,22 +2,32 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
+import Popover from 'react-bootstrap/Popover'
+
 
 class PhoneButton extends React.Component {
   render() {
     return (
       <OverlayTrigger
       key='bottom'
+      trigger="click"
       placement='bottom'
       delay={{ show: 750 }}
       overlay={
-        <Tooltip id="tooltip-bottom">
-          (610) 996-0580
-        </Tooltip>
+
+        <Popover id="popover-basic">
+        <Popover.Content>
+        <Button
+          variant="outline-danger"
+          href="tel:16109960580"
+          target="_blank">
+            (610) 996-0580
+        </Button>
+        </Popover.Content>
+        </Popover>
       }>
         <Button
           variant="outline-secondary"
-          href="tel:16109960580"
           target="_blank">
             <i className="fas fa-phone fa-lg"></i>
         </Button>

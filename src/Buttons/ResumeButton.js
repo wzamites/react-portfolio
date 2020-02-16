ed087838780ca299d60b5e1a5aa8f27e5b4fe842
wzamites <bj_zamites@comcast.net>
2020-02-15 22:45:35 -0500

    change resume button

--- src/Buttons/ResumeButton.js
+++ src/Buttons/ResumeButton.js
@@ -6,22 +6,12 @@ import Tooltip from 'react-bootstrap/Tooltip'
 class ResumeButton extends React.Component {
   render() {
     return (
-      <OverlayTrigger
-      key='bottom'
-      placement='bottom'
-      delay={{ show: 750 }}
-      overlay={
-        <Tooltip id="tooltip-bottom">
-          Resume
-        </Tooltip>
-        }>
         <Button
           variant="outline-secondary"
           href="https://drive.google.com/file/d/1crHmK_ONvLLEJnq5oHXtaTZ_m3Wxd1ZZ/view?usp=sharing"
           target="_blank">
-            <i className="fas fa-file fa-lg"></i>
+            <i className="fas fa-file-alt fa-lg"></i>
         </Button>
-      </OverlayTrigger>
     )
   }
 }

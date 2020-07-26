64ac1804163a906bf5e53139052efa9b21894719
BJ Zamites <wzamites@gmail.com>
2020-07-26 02:34:54 -0400

    add tooltip (no editor)

--- src/Buttons/ResumeButton.js
+++ src/Buttons/ResumeButton.js
@@ -3,15 +3,23 @@ import Button from 'react-bootstrap/Button'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
 
+const resumeTooltip = () => (
+  <Tooltip id="resume">
+    Resume
+  </Tooltip>
+);
+
 class ResumeButton extends React.Component {
   render() {
     return (
-        <Button
-          variant="outline-secondary"
-          href="https://drive.google.com/file/d/19kA09JMiLqfNtefxA35EYcpQIjzGfkTE/view?usp=sharing"
-          target="_blank">
-            <i className="fas fa-file-alt fa-lg"></i>
-        </Button>
+        <OverlayTrigger placement="bottom" overlay={resumeTooltip}>
+          <Button
+            variant="outline-secondary"
+            href="https://drive.google.com/file/d/19kA09JMiLqfNtefxA35EYcpQIjzGfkTE/view?usp=sharing"
+            target="_blank">
+              <i className="fas fa-file-alt fa-lg"></i>
+          </Button>
+        </OverlayTrigger>
     )
   }
 }
